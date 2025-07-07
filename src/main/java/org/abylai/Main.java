339044f90ad5102bcc672b33bf1668fc96e1f2d8
abylai-nurske <abylai.nurskenev@gmail.com>
2025-07-08 02:20:53 +0500

    Added Main (forgot to add to the last commit)

--- src/main/java/org/abylai/Main.java
+++ src/main/java/org/abylai/Main.java
@@ -4,27 +4,48 @@ package org.abylai;
 import org.abylai.writers.FloatWriter;
 import org.abylai.writers.IntegerWriter;
 import org.abylai.writers.StringWriter;
-import org.abylai.writers.Writer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            run(args);
-        } catch (Exception e) {
-            // TODO: Добавить нормальую обработку
-            e.printStackTrace();
-        }
+        run(args);
     }
 
-    private static void run(String[] args) throws Exception {
+    private static void run(String[] args) {
         Options options = ArgumentParser.parseArgs(args);
-        try (Writer writer = new FloatWriter(options)) {
-//            writer.write("1231412");
-//            writer.write("99912312");
-//            writer.write("-13212312");
-//            writer.write("1.528535047E-25");
 
-            writer.printStats();
+        try (
+            IntegerWriter integerWriter = new IntegerWriter(options);
+            FloatWriter floatWriter = new FloatWriter(options);
+            StringWriter stringWriter = new StringWriter(options);
+        ) {
+            for (Path in : options.inputFiles) {
+                try (BufferedReader br = Files.newBufferedReader(in, StandardCharsets.UTF_8)) {
+                    br.lines().forEach(line -> {
+                        switch (DataTypeDetector.detect(line)) {
+                            case INTEGER -> integerWriter.write(line);
+                            case FLOAT   -> floatWriter.write(line);
+                            case STRING  -> stringWriter.write(line);
+                        }
+                    });
+                } catch (NoSuchFileException e) {
+                    Logger.error("No such file", e);
+                } catch (AccessDeniedException e) {
+                    Logger.error("Access denied", e);
+                } catch (IOException e) {
+                    Logger.error("I/O error", e);
+                }
+            }
+            integerWriter.printStats();
+            floatWriter.printStats();
+            stringWriter.printStats();
         }
     }
 }
